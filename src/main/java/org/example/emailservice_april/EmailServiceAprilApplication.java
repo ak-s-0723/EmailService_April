a8baca5fba1fcb828e0df25888355c6bbb3ef2f0
khannaanurag1 <khannaanurag1@gmail.com>
2024-06-05 10:00:02 +0530

    Added support for sending email to user as soon as Kafka message is received

---
+++ src/main/java/org/example/emailservice_april/EmailServiceAprilApplication.java
@@ -0,0 +1,13 @@
+package org.example.emailservice_april;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServiceAprilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailServiceAprilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/emailservice_april/EmailServiceAprilApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.emailservice_april;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceAprilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
